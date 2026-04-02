44b9e840b37ccf58b34cd6c27bbae6126ea49b42
Wesley Leggo-Morrell <wes-lm@outlook.com>
2026-04-02 16:23:29 +0100

    Add a filter

--- src/Modal.cpp
+++ src/Modal.cpp
@@ -1,36 +1,51 @@
 //export RACK_DIR=/home/wes-l/Rack-SDK
 
 #include "plugin.hpp"
+#include <array>
 
-struct Modal : Module 
+static constexpr int NUM_MODES = 2;
+
+struct Filter
 {
-	enum ParamId 
+
+};
+
+struct Modal : Module
+{
+	enum ParamId
 	{
 		TEST_PARAM,
 		PARAMS_LEN
 	};
-	enum InputId 
+	enum InputId
 	{
-		TEST_INPUT,
+		AUDIO_INPUT,
 		INPUTS_LEN
 	};
-	enum OutputId 
+	enum OutputId
 	{
-		TEST_OUTPUT,
+		AUDIO_OUTPUT,
 		OUTPUTS_LEN
 	};
-	enum LightId 
+	enum LightId
 	{
 		TEST_LIGHT,
 		LIGHTS_LEN
 	};
 
 	// DSP
-	int srate = 44100;	
+	int srate = 48000;
+	
+	dsp::RCFilter mode;
+	//std::array<Filter, NUM_MODES> mode = {};
 
 	Modal() 
 	{
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
+		configInput(AUDIO_INPUT, "Input");
+		configOutput(AUDIO_OUTPUT, "Output");
+
+		//mode.setCutoffFreq(10);
 
 		onSampleRateChange();
 	}
@@ -60,7 +75,19 @@ struct Modal : Module
 	
 	void process(const ProcessArgs& args) override
 	{
+		float input = inputs[AUDIO_INPUT].getVoltage();
 
+		mode.setCutoffFreq(0.001);
+
+		mode.process(input);
+		float output = mode.lowpass();
+
+		//for (int i = 0; i < NUM_MODES; i++)
+		//{
+		//	//mode[i].
+		//}
+
+		outputs[AUDIO_OUTPUT].setVoltage(output);
 	}
 		
 };
@@ -71,13 +98,17 @@ struct ModalModuleWidget : ModuleWidget
 	ModalModuleWidget(Modal* module) 
 	{
 		setModule(module);
-		//setPanel(createPanel(asset::plugin(pluginInstance, "res/panels/wolfram.svg")));
+		setPanel(createPanel(asset::plugin(pluginInstance, "res/panels/modal.svg")));
 
 		// Srews
 		addChild(createWidget<ThemedScrew>(Vec(RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ThemedScrew>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ThemedScrew>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ThemedScrew>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		// Inputs
+		addInput(createInputCentered<BananutBlack>(mm2px(Vec(7.62f, 22.14f)), module, Modal::AUDIO_INPUT));
+		// Ouputs
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(7.62f, 99.852f)), module, Modal::AUDIO_OUTPUT));
 	}
 	
 	void appendContextMenu(Menu* menu) override
